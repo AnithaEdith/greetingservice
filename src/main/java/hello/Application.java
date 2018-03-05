02cc7362ced549e7fad04693f53b658525e38aa7
AnithaEdith <35328842+AnithaEdith@users.noreply.github.com>
2018-03-05 22:09:10 +0530

    Initial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,21 @@
+package hello;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
